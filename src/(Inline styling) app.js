35186e6716c8ddb27d42d044be8cfaa2639a4518
Varsha-PGDAV <91816943+Varsha-PGDAV@users.noreply.github.com>
2021-10-06 10:05:07 +0530

    Create (Inline styling) app.js

---
+++ src/(Inline styling) app.js
@@ -0,0 +1,13 @@
+ import React from 'react';  
+import ReactDOM from 'react-dom';  
+class App extends React.Component {  
+  render() {  
+    return (  
+      <div>  
+      <h1 style={{color: "Green"}}>Hello JavaTpoint!</h1>  
+      <p>Here, you can find all CS tutorials.</p>  
+      </div>  
+    );  
+  }  
+}  
+export default App; 
